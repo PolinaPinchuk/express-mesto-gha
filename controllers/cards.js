018cefb1202e9e342f729ac945c6aedf49a48b65
Polina Pinchuk <polinapinchuck@gmail.com>
2022-10-01 11:30:56 +0200

    express-mesto-gha

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,5 +1,6 @@
 const Card = require('../models/card');
 const { AccessError } = require('../errors/AccessError');
+const { NotFoundError } = require('../errors/NotFoundError');
 
 const ERROR_CODE = 400;
 const NOT_FOUND_ERROR = 404;
@@ -24,12 +25,38 @@ module.exports.createCard = (req, res) => {
     });
 };
 
+// module.exports.deleteCard = (req, res, next) => {
+//   Card.deleteOne({ _id: req.params.cardId })
+//     .orFail(() => {
+//       const error = new Error(`Карточка с таким _id ${req.params.cardId} не найдена`);
+//       error.statusCode = NOT_FOUND_ERROR;
+//       throw error;
+//     })
+//     .then((card) => {
+//       if (!card.owner.equals(req.user._id)) {
+//         return next(new AccessError('Карточка не может быть удалена'));
+//       }
+//       return card.remove()
+//         .then(() => {
+//           res.send({ message: 'Карточка удалена' });
+//         });
+//     })
+//     // .then((card) => res.send(card))
+//     .catch((err) => {
+//       if (err.name === 'CastError') {
+//         res.status(ERROR_CODE).send({ message: 'Некорректные данные' });
+//       } else if (err.statusCode === NOT_FOUND_ERROR) {
+//         res.status(NOT_FOUND_ERROR).send({ message: err.message });
+//       } else {
+//         res.status(DEFAULT_ERROR).send({ message: 'На сервере произошла ошибка' });
+//       }
+//     });
+// };
+
 module.exports.deleteCard = (req, res, next) => {
   Card.deleteOne({ _id: req.params.cardId })
     .orFail(() => {
-      const error = new Error(`Карточка с таким _id ${req.params.cardId} не найдена`);
-      error.statusCode = NOT_FOUND_ERROR;
-      throw error;
+      throw new NotFoundError(`Карточка с таким _id ${req.params.cardId} не найдена`);
     })
     .then((card) => {
       if (!card.owner.equals(req.user._id)) {
@@ -40,16 +67,7 @@ module.exports.deleteCard = (req, res, next) => {
           res.send({ message: 'Карточка удалена' });
         });
     })
-    // .then((card) => res.send(card))
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        res.status(ERROR_CODE).send({ message: 'Некорректные данные' });
-      } else if (err.statusCode === NOT_FOUND_ERROR) {
-        res.status(NOT_FOUND_ERROR).send({ message: err.message });
-      } else {
-        res.status(DEFAULT_ERROR).send({ message: 'На сервере произошла ошибка' });
-      }
-    });
+    .catch(next);
 };
 
 module.exports.likeCard = (req, res) => {
