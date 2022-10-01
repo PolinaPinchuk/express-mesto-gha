a9f364ea8615d44145f54184b1d01054095dd9b2
Polina Pinchuk <polinapinchuck@gmail.com>
2022-10-01 12:56:57 +0200

    express-mesto-gha

--- controllers/cards.js
+++ controllers/cards.js
@@ -25,36 +25,8 @@ module.exports.createCard = (req, res) => {
     });
 };
 
-// module.exports.deleteCard = (req, res, next) => {
-//   Card.deleteOne({ _id: req.params.cardId })
-//     .orFail(() => {
-//       const error = new Error(`Карточка с таким _id ${req.params.cardId} не найдена`);
-//       error.statusCode = NOT_FOUND_ERROR;
-//       throw error;
-//     })
-//     .then((card) => {
-//       if (!card.owner.equals(req.user._id)) {
-//         return next(new AccessError('Карточка не может быть удалена'));
-//       }
-//       return card.remove()
-//         .then(() => {
-//           res.send({ message: 'Карточка удалена' });
-//         });
-//     })
-//     // .then((card) => res.send(card))
-//     .catch((err) => {
-//       if (err.name === 'CastError') {
-//         res.status(ERROR_CODE).send({ message: 'Некорректные данные' });
-//       } else if (err.statusCode === NOT_FOUND_ERROR) {
-//         res.status(NOT_FOUND_ERROR).send({ message: err.message });
-//       } else {
-//         res.status(DEFAULT_ERROR).send({ message: 'На сервере произошла ошибка' });
-//       }
-//     });
-// };
-
 module.exports.deleteCard = (req, res, next) => {
-  Card.deleteOne({ _id: req.params.cardId })
+  Card.findById({ _id: req.params.cardId })
     .orFail(() => {
       throw new NotFoundError(`Карточка с таким _id ${req.params.cardId} не найдена`);
     })
@@ -74,9 +46,7 @@ module.exports.likeCard = (req, res) => {
   Card
     .findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
     .orFail(() => {
-      const error = new Error(`Карточка с таким _id ${req.params.cardId} не найдена`);
-      error.statusCode = NOT_FOUND_ERROR;
-      throw error;
+      throw new NotFoundError(`Карточка с таким _id ${req.params.cardId} не найдена`);
     })
     .then((card) => res.send(card))
     .catch((err) => {
@@ -93,9 +63,7 @@ module.exports.likeCard = (req, res) => {
 module.exports.dislikeCard = (req, res) => {
   Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
     .orFail(() => {
-      const error = new Error(`Карточка с таким _id ${req.params.cardId} не найдена`);
-      error.statusCode = NOT_FOUND_ERROR;
-      throw error;
+      throw new NotFoundError(`Карточка с таким _id ${req.params.cardId} не найдена`);
     })
     .then((card) => res.send(card))
     .catch((err) => {
